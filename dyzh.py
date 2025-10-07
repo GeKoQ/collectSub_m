73f4ad7b5e6963128ed45ecf2812f229819c2a61
GeKoQ <wurenjiaihaozhe@gmail.com>
2025-10-07 05:48:34 -0400

    Update dyzh.py

--- dyzh.py
+++ dyzh.py
@@ -26,8 +26,8 @@ from glob import glob
 USER_AGENTS = [
     'meta/0.2.0.5.Meta',
     'v2rayN/7.15.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15'
+    #'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    #'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15'
 ]
 
 TG_DOMAINS = [
